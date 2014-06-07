2d9a7c072a63131fe52cadf456bd08224c9fd07f
SydMontague <sydmontague@web.de>
2014-06-08 01:26:10 +0200

    Use changes in CLCore

--- src/main/java/de/craftlancer/economy/commands/MoneyCommandHandler.java
+++ src/main/java/de/craftlancer/economy/commands/MoneyCommandHandler.java
@@ -7,6 +7,7 @@ public class MoneyCommandHandler extends CommandHandler
 {
     public MoneyCommandHandler(CLEco plugin)
     {
+        super(plugin);
         registerSubCommand("take", new MoneyTakeCommand("cleco.commands.take", plugin, true), "remove");
         registerSubCommand("give", new MoneyGiveCommand("cleco.commands.give", plugin, true), "add");
         registerSubCommand("show", new MoneyShowCommand("cleco.commands.show", plugin, true));
